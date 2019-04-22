11eb4c25fe1e11512245e5f46b9cf99c6f38b3fe
Mattia Clementi <clementi@workup.it>
2019-04-22 08:55:27 +0200

    Basic configuration

---
+++ main.tf
@@ -0,0 +1,39 @@
+variable "hostname" {}
+variable "image" {}
+variable "private_key_path" {}
+variable "public_key_path" {}
+variable "region" {
+	default = "fra1"
+}
+variable "size" {
+	default = "s-1vcpu-1gb"
+}
+
+# Create a new ssh key
+#resource "digitalocean_ssh_key" "default" {
+#  name       = "${var.hostname}"
+#  public_key = "${file(var.public_key_path)}"
+#}
+
+# Create a new Web Droplet in the fra1 region
+resource "digitalocean_droplet" "web" {
+	image  = "${var.image}"
+	name   = "${var.hostname}"
+	region = "${var.region}"
+	size   = "${var.size}"
+	ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
+
+	provisioner "remote-exec" {
+		connection {
+			type     = "ssh"
+			user     = "root"
+			private_key = "${file(var.private_key_path)}"
+			timeout  = "2m"
+		}
+
+		inline = [
+			"apt-get -y update",
+			"apt-get -y install nginx",
+		]
+	}
+}
